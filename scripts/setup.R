fb9f7e4bc45f7232c7b7d6b4c4779dbbfbc54b57
=LauraLoPriore1997 <laura.lopriore@unil.ch>
2021-11-11 13:40:36 +0100

    Update R setup (libraries)
    Added new packages (lpSolver + shiny)

--- scripts/setup.R
+++ scripts/setup.R
@@ -6,11 +6,13 @@
 packages <- c(# general
   "base", "methods", "utils", "grDevices", "here", "tidyr",
   "gdata", "graphics", "stats", "lubridate", "dplyr", "ggplot2",
+  "lpSolve", # integer linear programming solver
   "knitr",  # report
   "tinytex", # latex equations
   "ganttrify", #Gant chart
   "fastDummies", # dummy variables
   "readxl", # open excel files
-  "magrittr" # use pipe operator
+  "magrittr", # use pipe operator
+  "shiny" # shiny application 
 )
 purrr::walk(packages, library, character.only = TRUE)
