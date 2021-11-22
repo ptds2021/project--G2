20536d9dd74633e00314f4cc4c664e8bd62b9460
alessiadipietro <alessia.dp@hotmail.com>
2021-11-22 21:22:18 +0100

    added shinyWidgets package

--- scripts/setup.R
+++ scripts/setup.R
@@ -13,6 +13,6 @@ packages <- c(# general
   "fastDummies", # dummy variables
   "readxl", # open excel files
   "magrittr", # use pipe operator
-  "shiny", "shinythemes" # shiny application 
+  "shiny", "shinythemes", "shinyWidgets" # shiny application 
 )
 purrr::walk(packages, library, character.only = TRUE)
