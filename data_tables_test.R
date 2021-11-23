821edb07a4f4fbca46322b27477fbf82c74c3e1a
alessiadipietro <alessia.dp@hotmail.com>
2021-11-23 13:51:13 +0100

    test table

--- data_tables_test.R
+++ data_tables_test.R
@@ -2,12 +2,12 @@ library(ggplot2)
 
 HEC_test <- HEC_sem3_BA[c(1,3,4,9),]
 
-d_new=data.frame(x1=HEC_sem3_BA$Day, 
-             x2=HEC_sem3_BA$Day+1, 
-             y1=HEC_sem3_BA$Start, 
-             y2=HEC_sem3_BA$End, 
-             t=HEC_sem3_BA$CORE, 
-             r=HEC_sem3_BA$Class)
+d_new=data.frame(x1=HEC_test$Day, 
+             x2=HEC_test$Day+1, 
+             y1=HEC_test$Start, 
+             y2=HEC_test$End, 
+             t=HEC_test$CORE, 
+             r=HEC_test$Class)
 
 
 fig_timetable <- ggplot() +
@@ -16,6 +16,6 @@ fig_timetable <- ggplot() +
             color="black", size=0, alpha=0.5) + 
   xlim(1, 6) +
   ylim(8.50, 19) +
-  labs(x="Days of the week", y="Time (h)") +
+  labs(x="Days of the week", y="Time") +
   theme(legend.position="none")
 
