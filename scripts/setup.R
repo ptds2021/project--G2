45a52c627e6410a49bf23d17f02fdbd86e5fc2b0
=LauraLoPriore1997 <laura.lopriore@unil.ch>
2021-11-29 09:59:15 +0100

    Removed graphe were not needed

--- scripts/setup.R
+++ scripts/setup.R
@@ -13,6 +13,6 @@ packages <- c(# general
   "fastDummies", # dummy variables
   "readxl", # open excel files
   "magrittr", # use pipe operator
-  "shiny", "shinythemes", "shinyWidgets" # shiny application 
+  "shiny", "shinythemes", "shinytest", "shinyWidgets" # shiny application 
 )
 purrr::walk(packages, library, character.only = TRUE)
