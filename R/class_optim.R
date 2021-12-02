b231a2a163e3bea0e44a635b18d451100906cf91
=LauraLoPriore1997 <laura.lopriore@unil.ch>
2021-12-02 21:46:31 +0100

    Improve code even further with improved class_optim

--- R/class_optim.R
+++ R/class_optim.R
@@ -4,27 +4,46 @@
 #' It can be used within the ShinyApp settings.
 #' @author Matteo Gross, Alessia Di Pietro, Martina Celic, Ana Gabriela Garcia, Laura Lo Priore
 #' @return Final choice of classes of user
-#' @param i Semester to be considered (from 1 to 3)
+#' @param s Semester to be considered (from 1 to 3)
 #' @param f.obj Objective function
 #' @param f.con Constraints matrix
 #' @param f.dir Sign of equality of constraints
-#' @param f.rhs Right hand side of constraints
+#' @param credits Total credits choosen by the user
+#' @param core Core credits choosen by the user#' 
+#' @param elective Elective credits choosen by the user
+#' @param moment Moments choosen by the user
+#' @param class Moments choosen by the user
 #' @import dplyr
 #' @importFrom lpSolve lp
 #' @import tidyverse
 #' @import shiny
 #' @export
-class_optim = function(i, f.obj, f.con, f.dir, f.rhs){
+class_optim = function(s, f.obj, f.con, f.dir, 
+                       credits, core, elective, moment, class){
+  
+  for (i in 1:length(moments_semester[[s]])) {
+    moments_constraints[[s]][i] <- moments_semester[[s]][i] %in% reactive(moment)()}
+  
+  for (i in 1:length(classes_semester[[s]])) {
+    classes_constraints[[s]][i] <- classes_semester[[s]][i] %in% reactive(class)()}
+
+  f.rhs <- c(
+    reactive(credits)(),                  # Credits constraint
+    reactive(core)(),                     # CORE Credits constraint
+    reactive(elective)(),                 # ELECTIVE Credits constraint
+    length(classes_constraints[[s]])*moments_constraints[[s]], # Chunk of the day constraints
+    rep.int(1, nb_of_timeslot[[s]]),      # Time constraints
+    classes_constraints[[s]])            # Classes constraints
 
   choice <- cbind(
     as.numeric(lp("min",
-                  f.obj[[i]],
-                  f.con[[i]],
-                  f.dir[[i]],
+                  f.obj[[s]],
+                  f.con[[s]],
+                  f.dir[[s]],
                   f.rhs,
                   all.bin = TRUE,
                   presolve = 1)$solution),
-    colnames(HEC_dummies_BA[[i]])) %>%
+    colnames(HEC_dummies_BA[[s]])) %>%
     as.data.frame() %>%
     mutate(Choice = V1, Class = V2)
 
