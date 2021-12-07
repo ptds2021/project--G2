3bb8c53839fd44c6f9c7ce7e96bb1067aa453281
=LauraLoPriore1997 <laura.lopriore@unil.ch>
2021-12-07 21:31:59 +0100

    Reindended code

--- inst/shiny-examples/timetables/server.R
+++ inst/shiny-examples/timetables/server.R
@@ -1,36 +1,71 @@
 server <- function(input, output, session) {
-  
   choice_1 <- eventReactive(input$submit_1,  {
-    hectimetables::class_optim(1, f.obj, f.con, f.dir,
-                               input$credits_1, 
-                               input$CORE_credits_1,
-                               input$ELECTIVE_credits_1, 
-                               input$Moments_1, 
-                               input$Classes_1)})
+    hectimetables::class_optim(
+      1,
+      f.obj,
+      f.con,
+      f.dir,
+      input$credits_1,
+      input$CORE_credits_1,
+      input$ELECTIVE_credits_1,
+      input$Moments_1,
+      input$Classes_1
+    )
+  })
   
-  output$timetable_1 <- renderTable({hectimetables::display_text_timetable(1, choice_1() )})
-  output$timetable_1_graph <- renderPlot({hectimetables::display_visual_timetable(1, choice_1() )})
+  output$timetable_1 <-
+    renderTable({
+      hectimetables::display_text_timetable(1, choice_1())
+    })
+  output$timetable_1_graph <-
+    renderPlot({
+      hectimetables::display_visual_timetable(1, choice_1())
+    })
   
   choice_2 <- eventReactive(input$submit_2, {
-    hectimetables::class_optim(2, f.obj, f.con, f.dir,
-                               input$credits_2, 
-                               input$CORE_credits_2,
-                               input$ELECTIVE_credits_2, 
-                               input$Moments_2, 
-                               input$Classes_2)})
+    hectimetables::class_optim(
+      2,
+      f.obj,
+      f.con,
+      f.dir,
+      input$credits_2,
+      input$CORE_credits_2,
+      input$ELECTIVE_credits_2,
+      input$Moments_2,
+      input$Classes_2
+    )
+  })
   
-  output$timetable_2 <- renderTable({hectimetables::display_text_timetable(2, choice_2() )})
-  output$timetable_2_graph <- renderPlot({hectimetables::display_visual_timetable(2, choice_2() )})
+  output$timetable_2 <-
+    renderTable({
+      hectimetables::display_text_timetable(2, choice_2())
+    })
+  output$timetable_2_graph <-
+    renderPlot({
+      hectimetables::display_visual_timetable(2, choice_2())
+    })
   
   choice_3 <- eventReactive(input$submit_3, {
-    hectimetables::class_optim(3, f.obj, f.con, f.dir,
-                               input$credits_3, 
-                               input$CORE_credits_3, 
-                               input$ELECTIVE_credits_3, 
-                               input$Moments_3, 
-                               input$Classes_3)})
+    hectimetables::class_optim(
+      3,
+      f.obj,
+      f.con,
+      f.dir,
+      input$credits_3,
+      input$CORE_credits_3,
+      input$ELECTIVE_credits_3,
+      input$Moments_3,
+      input$Classes_3
+    )
+  })
   
-  output$timetable_3 <- renderTable({hectimetables::display_text_timetable(3, choice_3() )})
-  output$timetable_3_graph <- renderPlot({hectimetables::display_visual_timetable(3, choice_3() )})
+  output$timetable_3 <-
+    renderTable({
+      hectimetables::display_text_timetable(3, choice_3())
+    })
+  output$timetable_3_graph <-
+    renderPlot({
+      hectimetables::display_visual_timetable(3, choice_3())
+    })
   
 }
--- inst/shiny-examples/timetables/ui.R
+++ inst/shiny-examples/timetables/ui.R
@@ -4,126 +4,198 @@ library(shinyWidgets)
 library(shiny)
 
 # Define UI
-ui <- navbarPage(theme = shinytheme("cerulean"),
-                 "HEC Lausanne - Timetable suggestions for Management students",
-                 tabPanel(
-                   "Semester 1",
-                   sidebarPanel(
-                     # ECTS choices
-                     actionButton(inputId = "submit_1", label = "Suggest timetable"),
-                     sliderInput(
-                       inputId = "credits_1",
-                       min = 0,
-                       max = 42,
-                       value = 30,
-                       label = "Total credits"),
-                     sliderInput(
-                       inputId = "CORE_credits_1",
-                       min = 0,
-                       max = 36,
-                       value = 18,
-                       label = "Mandatory credits"),
-                     sliderInput(
-                       inputId = "ELECTIVE_credits_1",
-                       min = 0,
-                       max = 24,
-                       value = 12,
-                       label = "Elective credits"),
-                     #Classes choices
-                     pickerInput(
-                       inputId = "Classes_1",
-                       label = "Classes:",
-                       choices = classes_semester[[1]],
-                       selected = classes_mandatory[[1]], # Refer to mandatory classes
-                       options = list(
-                         `actions-box` = TRUE, size = 10,
-                         `selected-text-format` = "count > 3"),
-                       multiple = TRUE),
-                     # Moments choices
-                     pickerInput(
-                       inputId = "Moments_1",
-                       label = "Day - AM/PM:",
-                       choices = moments_semester[[1]],
-                       selected = moments_semester[[1]],
-                       options = list(
-                         `actions-box` = TRUE, size = 10,
-                         `selected-text-format` = "count > 3"),
-                       multiple = TRUE)),
-                   # Show a plot of the generated distribution
-                   mainPanel(mainPanel(tabsetPanel(type = "tabs",
-                                                   tabPanel("Visual Timetable", plotOutput("timetable_1_graph")),
-                                                   tabPanel("More detailed Timetable", tableOutput("timetable_1"))), 
-                                       style='width: 100%'))),
-                 tabPanel(
-                   "Semester 2",
-                   sidebarPanel(
-                     # ECTS choices
-                     actionButton(inputId = "submit_2", label = "Suggest timetable"),
-                     sliderInput(inputId = "credits_2",
-                                 min = 0, max = 42, value = 30, label = "Total credits"),
-                     sliderInput(inputId = "CORE_credits_2",
-                                 min = 0, max = 36, value = 12, label = "Mandatory credits"),
-                     sliderInput(inputId = "ELECTIVE_credits_2",
-                                 min = 0, max = 24, value = 18, label = "Elective credits"),
-                     #Classes choices
-                     pickerInput(
-                       inputId = "Classes_2",
-                       label = "Classes:",
-                       choices = classes_semester[[2]],
-                       selected = classes_mandatory[[2]],
-                       options = list(
-                         `actions-box` = TRUE, size = 10,
-                         `selected-text-format` = "count > 3"),
-                       multiple = TRUE),
-                     # Moments choices
-                     pickerInput(
-                       inputId = "Moments_2",
-                       label = "Day - AM/PM:",
-                       choices = moments_semester[[2]],
-                       selected = moments_semester[[2]],
-                       options = list(
-                         `actions-box` = TRUE, size = 10,
-                         `selected-text-format` = "count > 3"),
-                       multiple = TRUE)),
-                   # Show a plot of the generated distribution
-                   mainPanel(mainPanel(tabsetPanel(type = "tabs",
-                                                   tabPanel("Visual Timetable", plotOutput("timetable_2_graph")),
-                                                   tabPanel("More detailed Timetable", tableOutput("timetable_2"))), 
-                                       style='width: 100%'))),
-                 tabPanel(
-                   "Semester 3",
-                   sidebarPanel(
-                     actionButton(inputId = "submit_3", label = "Suggest timetable"),
-                     # ECTS choices
-                     sliderInput(inputId = "credits_3",
-                                 min = 0, max = 42, value = 30, label = "Total credits"),
-                     sliderInput(inputId = "CORE_credits_3",
-                                 min = 0, max = 36, value = 12, label = "Mandatory credits"),
-                     sliderInput(inputId = "ELECTIVE_credits_3",
-                                 min = 0, max = 24, value = 18, label = "Elective credits"),
-                     
-                     #Classes choices
-                     pickerInput(
-                       inputId = "Classes_3",
-                       label = "Classes:",
-                       choices = classes_semester[[3]],
-                       selected = classes_mandatory[[3]],
-                       options = list(
-                         `actions-box` = TRUE, size = 10,
-                         `selected-text-format` = "count > 3"),
-                       multiple = TRUE),
-                     # Moments choices
-                     pickerInput(
-                       inputId = "Moments_3",
-                       label = "Day - AM/PM:",
-                       choices = moments_semester[[3]],
-                       selected = moments_semester[[3]],
-                       options = list(
-                         `actions-box` = TRUE, size = 10,
-                         `selected-text-format` = "count > 3"),
-                       multiple = TRUE)),
-                   # Show a plot of the generated distribution
-                   mainPanel(mainPanel(tabsetPanel(type = "tabs",
-                                                   tabPanel("Visual Timetable", plotOutput("timetable_3_graph")),
-                                                   tabPanel("More detailed Timetable", tableOutput("timetable_3"))), 
-                                       style='width: 100%'))))
+ui <- navbarPage(
+  theme = shinytheme("cerulean"),
+  "HEC Lausanne - Timetable suggestions for Management students",
+  tabPanel(
+    "Semester 1",
+    sidebarPanel(
+      # ECTS choices
+      actionButton(inputId = "submit_1", label = "Suggest timetable"),
+      sliderInput(
+        inputId = "credits_1",
+        min = 0,
+        max = 42,
+        value = 30,
+        label = "Total credits"
+      ),
+      sliderInput(
+        inputId = "CORE_credits_1",
+        min = 0,
+        max = 36,
+        value = 18,
+        label = "Mandatory credits"
+      ),
+      sliderInput(
+        inputId = "ELECTIVE_credits_1",
+        min = 0,
+        max = 24,
+        value = 12,
+        label = "Elective credits"
+      ),
+      #Classes choices
+      pickerInput(
+        inputId = "Classes_1",
+        label = "Classes:",
+        choices = classes_semester[[1]],
+        selected = classes_mandatory[[1]],
+        # Refer to mandatory classes
+        options = list(
+          `actions-box` = TRUE,
+          size = 10,
+          `selected-text-format` = "count > 3"
+        ),
+        multiple = TRUE
+      ),
+      # Moments choices
+      pickerInput(
+        inputId = "Moments_1",
+        label = "Day - AM/PM:",
+        choices = moments_semester[[1]],
+        selected = moments_semester[[1]],
+        options = list(
+          `actions-box` = TRUE,
+          size = 10,
+          `selected-text-format` = "count > 3"
+        ),
+        multiple = TRUE
+      )
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Visual Timetable", plotOutput("timetable_1_graph")),
+        tabPanel("More detailed Timetable", tableOutput("timetable_1"))
+      ),
+      style = 'width: 100%'
+    ))
+  ),
+  tabPanel(
+    "Semester 2",
+    sidebarPanel(
+      # ECTS choices
+      actionButton(inputId = "submit_2", label = "Suggest timetable"),
+      sliderInput(
+        inputId = "credits_2",
+        min = 0,
+        max = 42,
+        value = 30,
+        label = "Total credits"
+      ),
+      sliderInput(
+        inputId = "CORE_credits_2",
+        min = 0,
+        max = 36,
+        value = 12,
+        label = "Mandatory credits"
+      ),
+      sliderInput(
+        inputId = "ELECTIVE_credits_2",
+        min = 0,
+        max = 24,
+        value = 18,
+        label = "Elective credits"
+      ),
+      #Classes choices
+      pickerInput(
+        inputId = "Classes_2",
+        label = "Classes:",
+        choices = classes_semester[[2]],
+        selected = classes_mandatory[[2]],
+        options = list(
+          `actions-box` = TRUE,
+          size = 10,
+          `selected-text-format` = "count > 3"
+        ),
+        multiple = TRUE
+      ),
+      # Moments choices
+      pickerInput(
+        inputId = "Moments_2",
+        label = "Day - AM/PM:",
+        choices = moments_semester[[2]],
+        selected = moments_semester[[2]],
+        options = list(
+          `actions-box` = TRUE,
+          size = 10,
+          `selected-text-format` = "count > 3"
+        ),
+        multiple = TRUE
+      )
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Visual Timetable", plotOutput("timetable_2_graph")),
+        tabPanel("More detailed Timetable", tableOutput("timetable_2"))
+      ),
+      style = 'width: 100%'
+    ))
+  ),
+  tabPanel(
+    "Semester 3",
+    sidebarPanel(
+      actionButton(inputId = "submit_3", label = "Suggest timetable"),
+      # ECTS choices
+      sliderInput(
+        inputId = "credits_3",
+        min = 0,
+        max = 42,
+        value = 30,
+        label = "Total credits"
+      ),
+      sliderInput(
+        inputId = "CORE_credits_3",
+        min = 0,
+        max = 36,
+        value = 12,
+        label = "Mandatory credits"
+      ),
+      sliderInput(
+        inputId = "ELECTIVE_credits_3",
+        min = 0,
+        max = 24,
+        value = 18,
+        label = "Elective credits"
+      ),
+      
+      #Classes choices
+      pickerInput(
+        inputId = "Classes_3",
+        label = "Classes:",
+        choices = classes_semester[[3]],
+        selected = classes_mandatory[[3]],
+        options = list(
+          `actions-box` = TRUE,
+          size = 10,
+          `selected-text-format` = "count > 3"
+        ),
+        multiple = TRUE
+      ),
+      # Moments choices
+      pickerInput(
+        inputId = "Moments_3",
+        label = "Day - AM/PM:",
+        choices = moments_semester[[3]],
+        selected = moments_semester[[3]],
+        options = list(
+          `actions-box` = TRUE,
+          size = 10,
+          `selected-text-format` = "count > 3"
+        ),
+        multiple = TRUE
+      )
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Visual Timetable", plotOutput("timetable_3_graph")),
+        tabPanel("More detailed Timetable", tableOutput("timetable_3"))
+      ),
+      style = 'width: 100%'
+    ))
+  )
+)
